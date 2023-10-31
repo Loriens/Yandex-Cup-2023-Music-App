f60be6e05c6b608939f6dd4b52c966d1183cd54b
Vladislav Markov <vladmarkov@yandex-team.ru>
2023-10-31 23:08:03 +0300

    Add empty project

---
+++ YandexCup2023MusicApp/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  YandexCup2023MusicApp
+//
+//  Created by Vladislav Markov on 31.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ YandexCup2023MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexCup2023MusicApp
+//
+//  Created by Vladislav Markov on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
